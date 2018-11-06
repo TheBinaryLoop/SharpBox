52ee0fa52394cff4c6a05506ca3884b73c8364d6
Lukas Eßmann <l.essmann@directonline.de>
2018-11-06 07:53:31 +0100

    Added .editorconfig

--- SharpBox.CLI/Program.cs
+++ SharpBox.CLI/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SharpBox.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-namespace SharpBox.CLI {
-   class Program {
-      static void Main(string[] args) {
-      }
-   }
+        }
+    }
 }
