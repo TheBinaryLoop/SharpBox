619e7465194800f0e1e0c1bbef92f92280af9cde
Lukas Eßmann <l.essmann@directonline.de>
2018-11-07 13:23:52 +0100

    Fixed main entry point for SharpBox.SVC

--- SharpBox.SVC/Program.cs
+++ SharpBox.SVC/Program.cs
@@ -1,22 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+using SharpBox.SVC.Framework;
 
-namespace SharpBox.SVC {
-   static class Program {
-      /// <summary>
-      /// The main entry point for the application.
-      /// </summary>
-      static void Main() {
-         ServiceBase[] ServicesToRun;
-         ServicesToRun = new ServiceBase[]
-         {
-                new Service1()
-         };
-         ServiceBase.Run(ServicesToRun);
-      }
-   }
+namespace SharpBox.SVC
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            try
+            {
+                // If install was a command line flag, then run the installer at runtime.
+                if (args.Contains("-install", StringComparer.InvariantCultureIgnoreCase))
+                {
+                    WindowsServiceInstaller.RuntimeInstall<ServiceImplementation>();
+                }
+
+                // If uninstall was a command line flag, run uninstaller at runtime.
+                else if (args.Contains("-uninstall", StringComparer.InvariantCultureIgnoreCase))
+                {
+                    WindowsServiceInstaller.RuntimeUnInstall<ServiceImplementation>();
+                }
+
+                // Otherwise, fire up the service as either console or windows service based on UserInteractive property.
+                else
+                {
+                    var implementation = new ServiceImplementation();
+
+                    // If started from console, file explorer, etc, run as console app.
+                    if (Environment.UserInteractive)
+                    {
+                        ConsoleHarness.Run(args, implementation);
+                    }
+
+                    // Otherwise run as a windows service
+                    else
+                    {
+                        ServiceBase.Run(new WindowsServiceHarness(implementation));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ConsoleHarness.WriteToConsole(ConsoleColor.Red, "An exception occurred in Main(): {0}", ex);
+            }
+        }
+    }
 }
